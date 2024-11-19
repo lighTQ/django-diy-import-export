2b402f4e19cefd28ab57ea636a5a2f19177ef53f
lighTQ <lightqsvip@126.com>
2024-11-19 22:05:46 +0800

    。

--- api/config_view.py
+++ api/config_view.py
@@ -29,6 +29,7 @@ class MyModelViewSet(ModelViewSet):
 
     # 重写某些方法
     def list(self, request, *args, **kwargs):
+        print(self.action) #表示执行的是哪个方法
         response = super(MyModelViewSet, self).list(request, *args, **kwargs)
         res = {'result':response.data, 'msg':'查询成功', 'code':status.HTTP_200_OK}
         return Response(res)
@@ -36,7 +37,12 @@ class MyModelViewSet(ModelViewSet):
     # 自己扩展的methods:派生
     @action(detail=False, methods=['get'], url_path='method_name_in_url_path, default=method_name')
     def login(self,request):
+        return Response('登陆成功')
 
+    @action(detail=True, methods=['get'])
+    def login2(self,request,*args,**kwargs):
+        # 获取生成pk路径的pk参数
+        print(**kwargs)
         return Response('登陆成功')
 
 class ConfigModelView(MyModelViewSet):
